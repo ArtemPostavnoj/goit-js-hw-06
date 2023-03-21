e33ce315ecda5e5f79c6a5763b6a5b35ad7c27b4
ArtemPostavnoj <artempostavnoj@gmail.com>
2023-03-21 20:01:00 +0200

    commit 3

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,15 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsDoc = document.querySelector("#ingredients");
+
+let ingredientArr = [];
+ingredients.forEach(function(ingredient){
+const listItem = document.createElement("li");
+listItem.textContent = ingredient;
+listItem.classList = "item";
+return ingredientArr.push(listItem);
+});
+
+ingredientsDoc.append(...ingredientArr);
