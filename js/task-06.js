bd1d98cdada2b314ff392bea2f4f104c44818ba4
ArtemPostavnoj <artempostavnoj@gmail.com>
2023-03-24 17:46:17 +0200

    commit 14

--- js/task-06.js
+++ js/task-06.js
@@ -2,11 +2,11 @@ const input = document.querySelector('#validation-input');
 const dataLength = input.dataset.length;
 input.addEventListener("blur", () => {
     const inputLength = input.value.length;
-    if (dataLength <= inputLength) {
-        input.classList.add("valid");
-        input.classList.remove("invalid");
-    } else {
+    if (dataLength < inputLength || dataLength > inputLength) {
+        // input.classList.add("valid");
         input.classList.add("invalid");
-        input.classList.remove("valid");
+    } else {
+        input.classList.remove("invalid");
+        input.classList.add("valid");
     }
 });
