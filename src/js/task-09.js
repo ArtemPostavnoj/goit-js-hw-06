eac15c07e858b94586f55212a46e32eee6d8a4e0
ArtemPostavnoj <artempostavnoj@gmail.com>
2023-03-24 10:09:23 +0200

    commit 10

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,5 +1,11 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
+ const btnChange = document.querySelector(".change-color")
+ const body = document.body;
+btnChange.addEventListener('click', onBtnClick);
+function onBtnClick(){
+  body.style.backgroundColor = getRandomHexColor();
+  function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215)
+      .toString(16)
+      .padStart(6, 0)}`;
+  }
 }
